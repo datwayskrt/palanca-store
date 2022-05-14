e3564075115564a0f3ffb5b588fb0fa9bf56bb5f
whoisxnio <49740149+datwayskrt@users.noreply.github.com>
2022-05-14 17:02:54 +0100

    del
    extra

--- src/pages/AboutUs/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.div`
-    padding-top: ${props => (props.width >= 920) ? '100px' : '65px'};
-`;
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Home} from './Home'
-export {default as AboutUs} from './AboutUs'
-export {default as Contacts} from './Contacts'
-export {default as PageNotFound} from './PageNotFound'
